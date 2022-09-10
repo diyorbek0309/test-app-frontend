09acbd636948271b6b8cda4fcf3d76f8951c2728
Diyorbek <diyorbekolimov2000@gmail.com>
2022-09-10 15:15:07 +0500

    added utils and pages

---
+++ src/utils/setToken.ts
@@ -0,0 +1,15 @@
+import api from './api';
+
+const authToken = (token: string) => {
+  if (token) {
+    api.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+    localStorage.setItem('token', token);
+  } else {
+    delete api.defaults.headers.common['authorization'];
+
+    localStorage.removeItem('token');
+  }
+};
+
+export default authToken;
