8e0d7c6a9e08c5d0a7fb7889e96b9ebd56cbf14e
diyorbek0309 <diyorbekolimov2000@gmail.com>
2022-09-13 14:10:35 +0500

    added routes file

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { routes } from './routes';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path='/'>
+        <Route path={routes.home}>
           <Route index element={<Login />} />
-          <Route path='/register' element={<Register />} />
+          <Route path={routes.register} element={<Register />} />
         </Route>
       </Routes>
     </div>
